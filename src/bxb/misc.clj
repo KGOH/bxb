217b277152e20b156ef6d05eeee337d63395601f
Юрий Махоткин <kgofhedgehogs@gmail.com>
2019-06-21 17:48:05 +0200

    added credit to dissoc-in

--- src/bxb/misc.clj
+++ src/bxb/misc.clj
@@ -3,7 +3,8 @@
 (defn dissoc-in
   "Dissociates an entry from a nested associative structure returning a new
   nested structure. keys is a sequence of keys. Any empty maps that result
-  will not be present in the new structure."
+  will not be present in the new structure.
+  Credit: https://stackoverflow.com/a/14488425/6179231"
   [m [k & ks :as keys]]
   (if ks
     (if-let [nextmap (get m k)]
