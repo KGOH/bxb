b0e6a00d3bba6bbd3cf63d470f0196c265cb0b41
Юрий Махоткин <kgofhedgehogs@gmail.com>
2019-06-25 23:16:37 +0200

    refactor

--- src/bxb/core.clj
+++ src/bxb/core.clj
@@ -3,13 +3,13 @@
             [bxb.mutate-fns :refer :all]
             [utiliva.core :refer [keepcat]]))
 
-(defn- walk [search-fn soc-fn path]
+(defn- walk-path [search-fn soc-fn path]
   (loop [mutations []
          [first-p & rest-p :as path] path
          cur-prefix []]
     (cond
       (every? may-be-a-key? path)
-      {:path  (concat cur-prefix (map constantly path))
+      {:path      (concat cur-prefix (map constantly path))
        :mutations mutations}
 
       (may-be-a-key? first-p)
@@ -29,9 +29,9 @@
              (into first-p rest-p)
              (conj cur-prefix (search-fn cur-prefix (first first-p)))))))
 
-(defn- walk-path [search-fn get-fn assoc-fn dissoc-fn src dest]
-  (let [{get-value-path :path, dissoc-mutations :mutations} (walk search-fn dissoc-fn src)
-        {put-value-path :path, assoc-mutations  :mutations} (walk search-fn assoc-fn  dest)]
+(defn- interpret-template [search-fn get-fn assoc-fn dissoc-fn src dest]
+  (let [{get-value-path :path, dissoc-mutations :mutations} (walk-path search-fn dissoc-fn src)
+        {put-value-path :path, assoc-mutations  :mutations} (walk-path search-fn assoc-fn  dest)]
     (-> assoc-mutations
         (conj (assoc-fn put-value-path (get-fn get-value-path)))
         (conj (dissoc-fn get-value-path))
@@ -40,17 +40,19 @@
 (defn create-mutations
   "Creates mutations to transform data. Bidirectional"
   [search-fn get-fn assoc-fn dissoc-fn [from to] template]
-  (keepcat (fn [{src from, dest to}]
-             (when (and src dest) (walk-path search-fn get-fn assoc-fn dissoc-fn src dest)))
-           template))
+  (let [interpret-template*
+        (partial interpret-template search-fn get-fn assoc-fn dissoc-fn)]
+    (keepcat (fn [{src from, dest to}]
+               (when (and src dest) (interpret-template* src dest)))
+             template)))
 
 (defn mutate [mutations data]
   (reduce #(%2 %1)
           data
           mutations))
 
-(defn hmap-mutations [dir template]
-  (create-mutations hmap-search-fn hmap-get-fn hmap-assoc-fn hmap-dissoc-fn dir template))
+(def hmap-mutations
+  (partial create-mutations hmap-search-fn hmap-get-fn hmap-assoc-fn hmap-dissoc-fn))
 
-(defn sql-mutations [dir template]
-  (create-mutations sql-search-fn sql-get-fn sql-assoc-fn sql-dissoc-fn dir template))
+(def sql-mutations
+  (partial create-mutations sql-search-fn sql-get-fn sql-assoc-fn sql-dissoc-fn))
