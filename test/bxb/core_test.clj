c2b7b8679089aa8c397db6242c4e4bca8c254bda
Юрий Махоткин <kgofhedgehogs@gmail.com>
2019-06-26 13:20:26 +0200

    added more tests

--- test/bxb/core_test.clj
+++ test/bxb/core_test.clj
@@ -3,40 +3,119 @@
             [bxb.core :refer :all]))
 
 (deftest test-stu3-r4
-  (def VisionPrescription-stu3
-    {:resourceType "VisionPrescription"
-     :reason "ups"
-     :extension [{:url "http://hl7.org/fact"
-                  :value {:code "fucct"}}]
-     :dispense {:prism 5
-                :base 1}})
+  (def test-cases
+    [{:desc     "hashmap mutation %s->%s nested name remap"
+      :spec     [:stu3 :r4]
+      :stu3     {:resourceType "VisionPrescription"
+                 :reason       "ups"
+                 :dispense     {:prism 5
+                                :base  1}}
+      :r4       {:resourceType      "VisionPrescription"
+                 :code              "ups"
+                 :lensSpecification {:amount 5
+                                     :base 1}}
+      :template [{:stu3 [:reason]
+                  :r4   [:code]}
+                 {:stu3 [:dispense :prism]
+                  :r4   [:lensSpecification :amount]}
+                 {:stu3 [:dispense :base]
+                  :r4   [:lensSpecification :base]}]}
 
-  (def VisionPrescription-r4
-    {:resourceType "VisionPrescription"
-     :extension [{:url "http://hl7.org/fact"
-                  :value {:code "fucct"}}
-                 {:url "http://hl7.org/reason"
-                  :value {:code "ups"}}]
-     :lensSpecification {:amount 5
-                          :base 1}})
+     {:desc     "hashmap mutation %s->%s (un)nesting name remap"
+      :spec     [:stu3 :r4]
+      :stu3     {:resourceType "VisionPrescription"
+                 :reason       "ups"
+                 :dispense     {:prism 5
+                                :base  1}}
+      :r4       {:resourceType "VisionPrescription"
+                 :code         "ups"
+                 :amount       5
+                 :base         1}
+      :template [{:stu3 [:reason]
+                  :r4   [:code]}
+                 {:stu3 [:dispense :prism]
+                  :r4   [:amount]}
+                 {:stu3 [:dispense :base]
+                  :r4   [:base]}]}
 
-  (def VisionPrescription-template
-    [{:stu3 [:reason]
-      :r4   [:extension [{:url "http://hl7.org/reason"}] :value :code]}
-     {:stu3 [:dispense :prism]
-      :r4   [:lensSpecification :amount]}
-     {:stu3 [:dispense :base]
-      :r4   [:lensSpecification :base]}])
+     {:desc     "hashmap mutation %s->%s with adding const by the path"
+      :spec     [:stu3 :r4]
+      :stu3     {:resourceType "VisionPrescription"
+                 :reason       "ups"
+                 :dispense     {:prism 5
+                                :base 1}}
+      :r4       {:resourceType      "VisionPrescription"
+                 :extension         {:url "http://hl7.org/reason"
+                                     :value {:code "ups"}}
+                 :lensSpecification {:amount 5
+                                     :base 1}}
+      :template [{:stu3 [:reason]
+                  :r4   [:extension {:url "http://hl7.org/reason"} :value :code]}
+                 {:stu3 [:dispense :prism]
+                  :r4   [:lensSpecification :amount]}
+                 {:stu3 [:dispense :base]
+                  :r4   [:lensSpecification :base]}]}
 
-  (testing "hashmap mutation forwards stu3->r4"
-    (is (= VisionPrescription-r4
-           (mutate (hmap-mutations [:stu3 :r4] VisionPrescription-template)
-                   VisionPrescription-stu3))))
+     {:desc "hashmap mutation forwards %s->%s with creating extension vec"
+      :spec [:stu3 :r4]
+      :stu3 {:resourceType "VisionPrescription"
+             :reason       "ups"
+             :dispense     {:prism 5
+                            :base  1}}
+      :r4   {:resourceType      "VisionPrescription"
+             :extension         [{:url   "http://hl7.org/reason"
+                                  :value {:code "ups"}}]
+             :lensSpecification {:amount 5
+                                 :base   1}}
+      :template [{:stu3 [:reason]
+                  :r4   [:extension [{:url "http://hl7.org/reason"}] :value :code]}
+                 {:stu3 [:dispense :prism]
+                  :r4   [:lensSpecification :amount]}
+                 {:stu3 [:dispense :base]
+                  :r4   [:lensSpecification :base]}]}
 
-  (testing "hashmap mutation backwards stu3<-r4"
-    (is (= VisionPrescription-stu3
-           (mutate (hmap-mutations [:r4 :stu3] VisionPrescription-template)
-                   VisionPrescription-r4)))))
+     {:desc "hashmap mutation forwards %s->%s with existing extension vec"
+      :spec [:stu3 :r4]
+      :stu3 {:resourceType "VisionPrescription"
+             :reason "ups"
+             :extension [{:url "http://hl7.org/fact"
+                          :value {:code "fucct"}}]
+             :dispense {:prism 5
+                        :base 1}}
+      :r4   {:resourceType "VisionPrescription"
+             :extension [{:url "http://hl7.org/fact"
+                          :value {:code "fucct"}}
+                         {:url "http://hl7.org/reason"
+                          :value {:code "ups"}}]
+             :lensSpecification {:amount 5
+                                 :base 1}}
+      :template [{:stu3 [:reason]
+                  :r4   [:extension [{:url "http://hl7.org/reason"}] :value :code]}
+                 {:stu3 [:dispense :prism]
+                  :r4   [:lensSpecification :amount]}
+                 {:stu3 [:dispense :base]
+                  :r4   [:lensSpecification :base]}]}
+     {:desc     "hashmap mutation %s->%s remap with missing source"
+      :spec     [:stu3 :r4]
+      :stu3     {:resourceType "VisionPrescription"
+                 :reason       "ups"}
+      :r4       {:resourceType      "VisionPrescription"
+                 :code              "ups"}
+      :template [{:stu3 [:reason]
+                  :r4   [:code]}
+                 {:stu3 [:dispense :prism]
+                  :r4   [:lensSpecification :amount]}
+                 {:stu3 [:dispense :base]
+                  :r4   [:lensSpecification :base]}]}])
+
+  (mapv (fn [{:keys [desc spec template] :as t}]
+          (mapv (fn [[from to]]
+                  (testing (format desc from to)
+                    (is (= (to t)
+                           (mutate (hmap-mutations [from to] template)
+                                   (from t))))))
+                [spec (reverse spec)]))
+        test-cases))
 
 (comment
   (run-tests)
