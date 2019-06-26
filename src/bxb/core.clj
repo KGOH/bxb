e2b48a79ca08dbdf53681cf26e33409e6e299916
Юрий Махоткин <kgofhedgehogs@gmail.com>
2019-06-26 08:28:41 +0200

    refactor

--- src/bxb/core.clj
+++ src/bxb/core.clj
@@ -3,39 +3,44 @@
             [bxb.mutate-fns :refer :all]
             [utiliva.core :refer [keepcat]]))
 
-(defn- walk-path [search-fn soc-fn path]
-  (loop [mutations []
+(defn- walk-path [search-fn path]
+  (loop [walked-paths []
          [first-p & rest-p :as path] path
          cur-prefix []]
     (cond
       (every? may-be-a-key? path)
-      {:path      (concat cur-prefix (map constantly path))
-       :mutations mutations}
+      (conj walked-paths
+            [(concat cur-prefix (map constantly path))])
 
       (may-be-a-key? first-p)
-      (recur mutations
+      (recur walked-paths
              rest-p
              (conj cur-prefix (constantly first-p)))
 
       (map? first-p)
-      (recur (conj mutations (soc-fn cur-prefix (constantly first-p)))
+      (recur (conj walked-paths [cur-prefix (constantly first-p)])
              rest-p
              cur-prefix)
 
       (and (sequential? first-p)
            (single-elem? first-p)
            (map? (first first-p)))
-      (recur mutations
+      (recur walked-paths
              (into first-p rest-p)
              (conj cur-prefix (search-fn cur-prefix (first first-p)))))))
 
 (defn- interpret-template [search-fn get-fn assoc-fn dissoc-fn src dest]
-  (let [{get-value-path :path, dissoc-mutations :mutations} (walk-path search-fn dissoc-fn src)
-        {put-value-path :path, assoc-mutations  :mutations} (walk-path search-fn assoc-fn  dest)]
-    (-> assoc-mutations
-        (conj (assoc-fn put-value-path (get-fn get-value-path)))
-        (conj (dissoc-fn get-value-path))
-        (into dissoc-mutations))))
+  (let [dissoc-paths (walk-path search-fn src)
+        assoc-paths  (walk-path search-fn dest)
+        put-value-path (first (peek assoc-paths))
+        get-value-path (first (peek dissoc-paths))]
+    (concat
+      (map (partial apply assoc-fn)
+           (conj (pop assoc-paths)
+                 [put-value-path (get-fn get-value-path)]))
+      (map (partial apply dissoc-fn)
+           (conj (pop dissoc-paths)
+                 [get-value-path (get-fn get-value-path)])))))
 
 (defn create-mutations
   "Creates mutations to transform data. Bidirectional"
