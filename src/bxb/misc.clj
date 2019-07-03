1eca1b1502ebd23f17da6126eaf43c9a37db8eff
Юрий Махоткин <kgofhedgehogs@gmail.com>
2019-07-04 00:53:46 +0200

    fixed trying to dissoc from string

--- src/bxb/misc.clj
+++ src/bxb/misc.clj
@@ -64,14 +64,17 @@
   will not be present in the new structure.
   Credit: https://stackoverflow.com/a/14488425/6179231"
   ([m [k & ks :as keys]]
-   (if ks
-     (if-let [nextmap (get m k)]
-       (let [newmap (dissoc-in nextmap ks)]
-         (if (seq newmap)
-           (assoc m k newmap)
-           (dissocv m k)))
-       m)
-     (dissocv m k))))
+   (if-not (or (map? m)
+               (sequential? m))
+     m
+     (if ks
+       (if-let [nextmap (get m k)]
+         (let [newmap (dissoc-in nextmap ks)]
+           (if (seq newmap)
+             (assoc m k newmap)
+             (dissocv m k)))
+         m)
+       (dissocv m k)))))
 
 (defn load-edn
   "Load edn from an io/reader source (filename or io/resource).
