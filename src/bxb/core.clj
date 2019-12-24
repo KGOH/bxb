26584a4f9c6ed38d2c286bc1b551fbbf4b6233a7
KgOfHedgehogs <kgofhedgehogs@gmail.com>
2019-12-24 20:14:26 +0200

    Add vec to sequential cmp
    Co-authore-by: Panthevm <panthevm@yandex.ru>

--- src/bxb/core.clj
+++ src/bxb/core.clj
@@ -28,7 +28,7 @@
 
       (and (sequential? first-p)
            (misc/single-elem? first-p))
-      (let [ffp (first first-p)]
+      (let [[ffp] first-p]
         (cond
           (map? ffp)
           (recur (into first-p rest-p)
@@ -54,7 +54,7 @@
       (concat [(assoc-fn bxb_get_buffer (get-fn get-value-path))]
               (map (partial apply assoc-fn) assoc-const-paths-vals)
               [(assoc-fn  put-value-path (get-fn bxb_get_buffer))]
-              (when (not= src (take (count src) dest))
+              (when (not= (vec (take (count src) dest)) (vec src))
                 [(dissoc-fn get-value-path (get-fn bxb_get_buffer))])
               (map (partial apply dissoc-fn) dissoc-const-paths-vals)
               [(dissoc-fn bxb_get_buffer (get-fn bxb_get_buffer))])
